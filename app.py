7d8a2184be4234c05900acda861561abbd388a84
Werner <wstablein@hotmail.com>
2024-10-01 15:26:47 +0200

    Build individual .py script files in app_pages. Build summary_page_body function. Test render locally

--- app.py
+++ app.py
@@ -1,10 +1,23 @@
 import streamlit as st
 from app_pages.multipage import MultiPage
-from app_pages.test_page import test
 
+st.set_page_config(page_title="Heritage Housing Sale Price Predictor", page_icon="🖥️")
 
+# the scripts for the five pages are loaded here
+from app_pages.summary_page import summary_page_body
+# from app_pages.correlation_analysis_page import correlation_analysis_page
+# from app_pages.sales_price_prediction_page import sales_price_prediction_page
+# from app_pages.project_hypothesis_page import project_hypothesis_page
+# from app_pages.ml_price_prediction import ml_price_prediction_page
+
+# The app is instantiated here
 app = MultiPage(app_name="Heritage Housing Sale Price Predictor")
 
-app.add_page("Test Page", test)
+# App pages are added here with .add_page()
+app.add_page("Project Overview", summary_page_body)
+# app.add_page("Correlation Analysis", correlation_analysis_page)
+# app.add_page("Sale Price Prediction", sales_price_prediction_page)
+# app.add_page("Hypothesis Validation", project_hypothesis_page)
+# app.add_page("Machine Learning Model", ml_price_prediction_page)
 
 app.run()
---
+++ app_pages/summary_page.py
@@ -0,0 +1,47 @@
+import streamlit as st
+
+def summary_page_body():
+    """
+    Displays contents of the project summary page.
+    """
+    st.write("# Project Overview")
+
+    st.info(
+        "**Project Background**\n\n"
+        "Our client has inherited four houses in Ames, Iowa, "
+        "and asked for our assistance to maximize the sales price. "
+        "To that end, a Machine Learning model and regression "
+        "algorithms were created to provide the best possible price "
+        "indication for the property inherited."
+    )
+
+    st.write("# Project Dataset")
+
+    st.info(
+        "**Dataset Source**\n\n"
+        "The dataset is sourced from Kaggle, with approximately 1,500 records "
+        "of housing sales in Ames, Iowa. Each record includes 23 features that "
+        "describe the house profile. Features include information about the "
+        "floor area, basement, and existence of a garage."
+    )
+
+    st.write("# Business Requirements")
+
+    st.success(
+        "**Project Objectives**\n\n"
+        "The project has two primary objectives:\n"
+        "* **BR1**: Discover and analyze how house attributes correlate with "
+        "sales price supported by data visualizations to show the relationship.\n"
+        "* **BR2**: Development of a prediction model to estimate the sale prices "
+        "of the inherited houses and other properties in Ames, Iowa."
+    )
+    
+    st.write("# Additional Information")
+
+    st.write(
+        "* For more information on this project, please visit the "
+        "[README file](https://github.com/Werner-Staeblein/Project-5/blob/main/README.md)."
+    )
+
+# Call the function to display the content on the Streamlit page
+summary_page_body()
