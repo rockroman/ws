5d11710e310ad58d27906725a53d713cdde032e0
Werner <wstablein@hotmail.com>
2024-11-20 17:14:33 +0100

    Add pipeline steps and best features list and best features plot to ml_price_prediction.py

--- app_pages/ml_price_prediction.py
+++ app_pages/ml_price_prediction.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import pandas as pd
 import matplotlib.pyplot as plt
+from sklearn.metrics import r2_score
 from src.data_management import load_ames_data, load_pkl_file
 
 
@@ -19,16 +20,37 @@ def ml_price_prediction_page():
     y_test =  pd.read_csv(f"outputs/ml_pipeline/predict_price/{version}/y_test.csv")
 
  
-
     st.write("### ML Pipeline: Predict House Price")    
-    # display pipeline training summary conclusions
-    st.info(
-        f"* To answer BR2, a Regressor model was trained with a pipeline. The goald was\
-             an accuracy of at least 0.75 in predicting the sales price of a property that has a set of\
-                 attributes. \n"
-        f"* The pipeline performance for the best model on the train and test set is\
-             R2 == 0.XXXXXXXXX and R2 == 0.XXXXXXXXXXXX respectively.\n"
-        f"* The pipeline steps, best features determined, feature importance plot, pipeline performance and regression performance are shown below. "
-       )
+        
+    st.write(
+    f"* To answer BR2, a Regressor model was trained with a pipeline. The goal was "
+    f"an accuracy of at least 0.75 in predicting the sales price of a property that has a set of "
+    f"attributes. The R2 for the train set and test set are as follows:"
+    )
+
+    st.success(f"Train Set R2 = 0.84")
+    st.success(f"Test Set R2 = 0.77")
+
+    st.write(f"* Both the Train Set R2 of 0.84 and the Test Set R2 of 0.77 exceed the required threshold of 0.75, indicating a successful model performance on both sets")
+
+    st.write(
+    f"* The pipeline steps, best features determined, feature importance plot, pipeline performance and "
+    f"regression performance are shown below."
+    )    
+    
     st.write("---")
     
+    # Add the pipeline steps
+    st.write("* ML pipeline to predict sales prices of houses ")
+    st.code(price_pipe)
+    st.write("---")
+
+    # display the five best features
+    st.write("* The features the model was trained and their importance")
+    st.write(X_train.columns.to_list())
+    st.image(price_feat_importance)
+    st.write("---")
+
+
+    st.write("### Pipeline Performance")
+
