1485606b9a30f048e2764f3048edb3476aeca7aa
Werner <wstablein@hotmail.com>
2024-11-21 17:03:09 +0100

    Add heatmap_pps and heatmap_corr and CaclualteCorrAndPPS to correlation_analysis_page.py to show heatmaps

--- app_pages/correlation_analysis_page.py
+++ app_pages/correlation_analysis_page.py
@@ -1,12 +1,12 @@
-import plotly.express as px
-import numpy as np
-from feature_engine.discretisation import ArbitraryDiscretiser
 import streamlit as st
 from src.data_management import load_ames_data
-
 import matplotlib.pyplot as plt
 import seaborn as sns
+import ppscore as pps
 sns.set_style("whitegrid")
+from feature_engine.discretisation import ArbitraryDiscretiser
+import numpy as np
+import plotly.express as px
 
 
 def correlation_analysis_page():
@@ -68,6 +68,11 @@ def correlation_analysis_page():
     # load data frame
     df_eda = df.filter(vars_to_study + ['SalePrice'])
 
+    # Show heatmaps
+    if st.checkbox("Show Correlation and PPS Heatmaps"):
+        df_corr_pearson, df_corr_spearman, pps_matrix = CalculateCorrAndPPS(df_eda)
+        DisplayCorrAndPPS(df_corr_pearson, df_corr_spearman, pps_matrix, CorrThreshold=0.4, PPS_Threshold=0.2)
+
     # function that will lead to a display of plots of the variables
     def scatter_plot_for_eda(df, col, target_var):
         fig = plt.figure(figsize=(12, 6))
@@ -95,3 +100,68 @@ def correlation_analysis_page():
     # Plots per variable
     if st.checkbox("Variables Plots - visual analysis"):
         variables_plots(df_eda)
+        
+def heatmap_corr(df, threshold, figsize=(20,12), font_annot=8):
+    """
+    Function to create heatmap using correlations.
+    """
+    if len(df.columns) > 1:
+        mask = np.zeros_like(df, dtype=bool)
+        mask[np.triu_indices_from(mask)] = True
+        mask[abs(df) < threshold] = True
+
+        fig, axes = plt.subplots(figsize=figsize)
+        sns.heatmap(df, annot=True, xticklabels=True, yticklabels=True,
+                    mask=mask, cmap='viridis', annot_kws={"size": font_annot}, ax=axes,
+                    linewidth=0.5)
+        axes.set_yticklabels(df.columns, rotation=0)
+        plt.ylim(len(df.columns), 0)
+        st.pyplot(fig)
+
+
+def heatmap_pps(df, threshold, figsize=(20,12), font_annot=8):
+    """
+    Function to create heatmap with pps.
+    """
+    if len(df.columns) > 1:
+        mask = np.zeros_like(df, dtype=bool)
+        mask[abs(df) < threshold] = True
+
+        fig, ax = plt.subplots(figsize=figsize)
+        ax = sns.heatmap(df, annot=True, xticklabels=True,yticklabels=True,
+                         mask=mask, cmap='rocket_r', annot_kws={"size": font_annot},
+                         linewidth=0.05, linecolor='grey')
+
+        plt.ylim(len(df.columns), 0)
+        st.pyplot(fig)
+
+
+def CalculateCorrAndPPS(df):
+  """
+  Function for calculation of correlations and pps.
+  """
+  df_corr_spearman = df.corr(method="spearman")
+  df_corr_spearman.name = 'corr_spearman'
+  df_corr_pearson = df.corr(method="pearson")
+  df_corr_pearson.name = 'corr_pearson'
+
+  pps_matrix_raw = pps.matrix(df)
+  pps_matrix = pps_matrix_raw.filter(['x', 'y', 'ppscore']).pivot(columns='x', index='y', values='ppscore')
+
+  pps_score_stats = pps_matrix_raw.query("ppscore < 1").filter(['ppscore']).describe().T
+  print(pps_score_stats.round(3))
+
+  return df_corr_pearson, df_corr_spearman, pps_matrix
+
+
+def DisplayCorrAndPPS(df_corr_pearson, df_corr_spearman, pps_matrix,CorrThreshold,PPS_Threshold,
+                      figsize=(20,12), font_annot=8 ):
+  """
+  Function to display the correlations and pps.
+  """
+
+  heatmap_corr(df=df_corr_spearman, threshold=CorrThreshold, figsize=figsize, font_annot=font_annot)
+
+  heatmap_corr(df=df_corr_pearson, threshold=CorrThreshold, figsize=figsize, font_annot=font_annot)
+
+  heatmap_pps(df=pps_matrix,threshold=PPS_Threshold, figsize=figsize, font_annot=font_annot)
