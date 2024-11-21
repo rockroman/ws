ebf9c7162e8d6386c1c89abf4d8e9695f8f85e83
Werner <wstablein@hotmail.com>
2024-11-21 11:42:33 +0100

    Add predict_price function to predictive_analysis_functions and import to streamlit app page sales_price_prediction_page

--- app_pages/sales_price_prediction_page.py
+++ app_pages/sales_price_prediction_page.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import pandas as pd
 from src.data_management import load_inherited_houses_data, load_pkl_file
-from src.machine_learning.predictive_analysis_functions import predict_inherited_house_price
+from src.machine_learning.predictive_analysis_functions import predict_inherited_house_price, predict_price
 
 def sales_price_prediction_page():
     # Load the files for prediction
--- src/machine_learning/predictive_analysis_functions.py
+++ src/machine_learning/predictive_analysis_functions.py
@@ -12,3 +12,18 @@ def predict_inherited_house_price(X_inherited, house_features, price_pipeline):
 	this_price = price_prediction_inherited[0]
 		
 	return this_price
+
+
+def predict_price(X_live, house_features, price_pipeline):
+
+	# The features related to this pipeline
+	X_live_price = X_live.filter(house_features)
+
+	# prediction
+	price_prediction = price_pipeline.predict(X_live_price)
+		
+	statement = (
+			f"* The value of your house is: **${round(price_prediction[0])}**.\n\n"
+			)
+	
+	st.write(statement)
